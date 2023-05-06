aceb0db421c30ed9f6e47d0a71ab985f7bddc793
isaacscarboro <isaacscarboro99@gmail.com>
2023-05-06 13:56:45 -0400

    Big commit to fix lots of issues
    I moved files around to be in the places that they were looking for moved to a template based design so flask would run the cs and added a todo for the last big issue the login

--- FinalProject3155/app.py
+++ FinalProject3155/app.py
@@ -1,14 +1,14 @@
 from flask import Flask, render_template, request, redirect, url_for
 import mysql.connector
 
-app = Flask(__name__)
+app = Flask(__name__,template_folder='templates', static_folder='staticFiles')
 
 # connect to the MySQL database
 db = mysql.connector.connect(
   host="localhost",
-  user="yourusername",
-  password="yourpassword",
-  database="yourdatabase"
+  user="root",
+  password="1234",
+  database="testbase"
 )
 
 @app.route("/")
@@ -32,6 +32,19 @@ def create_post():
     else:
         # show the form to create a new post
         return render_template("blogs.html")
+@app.route("/signInUp", methods=["GET", "POST"])
+def create_account():
+    if request.method == "POST":
+        # todo add accounts to database this includes a check here to see if it is a login or a sign up and the logic to handle either
+        return render_template("signInUp.html")
+    else:
+        # show the show the signup form
+        return render_template("signInUp.html")
+    
+@app.route("/vision", methods=["GET", "POST"])
+def show_vision():
+        # show the show the vision page
+        return render_template("vision.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
